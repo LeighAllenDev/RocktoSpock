23a93b7a6f26a3bad39e7c5a9b0d6c69ba411cc5
LeighAllenDev <140525595+LeighAllenDev@users.noreply.github.com>
2023-10-06 16:15:52 +0100

    feat: add control divs to script

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,3 +4,12 @@ const userScore_span = document.getElementById('user-score');
 const cpuScore_span = document.getElementById('cpu-score');
 const scoreBoard_div = document.getElementsByClassName('scores');
 const results_p = document.querySelector('.results > p');
+/**
+ * Game button divs 
+ */
+const rock_div = document.getElementById('Rock');
+const paper_div = document.getElementById('Paper');
+const scissors_div = document.getElementById('Scissors');
+const lizard_div = document.getElementById('Lizard');
+const spock_div = document.getElementById('Spock');
+
