ca4d37e4135177b3eb0b923d19f6d7d4694bf8a9
LeighAllenDev <140525595+LeighAllenDev@users.noreply.github.com>
2023-10-06 16:12:23 +0100

    feat: add scores and results divs to dom

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,3 +2,5 @@ let userScore = 0;
 let cpuScore = 0;
 const userScore_span = document.getElementById('user-score');
 const cpuScore_span = document.getElementById('cpu-score');
+const scoreBoard_div = document.getElementsByClassName('scores');
+const results_p = document.querySelector('.results > p');
